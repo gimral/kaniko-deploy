e1d77284628d61558ce8b383e79319d3d7d8cbf9
gokhanimral <gimral@gmail.com>
2021-09-27 20:25:07 +0400

    cache maven and images

--- helm/kaniko/create.sh
+++ helm/kaniko/create.sh
@@ -1,2 +1,4 @@
-kubectl create -f pv.yaml
-kubectl create -f pvc.yaml   
+kubectl create -f maven-pv.yaml
+kubectl create -f maven-pvc.yaml
+kubectl create -f kaniko-cache-pv.yaml
+kubectl create -f kaniko-cache-pvc.yaml 
